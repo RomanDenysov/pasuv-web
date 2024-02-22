63115467dd455c070dab9d4d4814ae63633d0b75
Roman_home_pc <romandenysovsk@gmail.com>
2024-02-23 00:01:24 +0100

    init commit

---
+++ src/lib/text-data.ts
@@ -0,0 +1,163 @@
+export const textData = {
+	homepage: {
+		hero: {
+			title: "Kvalifikovaní a skúsení pracovníci pre vaše projekty",
+			content:
+				"Máte nedostatok kvalifikovaného personálu na stavebnom projekte, alebo potrebujete urýchliť jeho realizáciu? Sme pripravení čo najskôr k vám vyslať potrebný počet skúsených pracovníkov.",
+			button: "Začnime spolupracovať",
+		},
+		intro: {
+			content:
+				"Už od roku 2013 spolupracujeme na rôznych stavebných projektoch. Špecializujeme sa na priemyselnú aj bytovú výstavbu, stavbu hotelov, škôl a komerčných objektov.",
+			info: "Elektrikári, murári, vodoinštalatéri, stavební pracovníci a ďalšie profesie",
+			img: "construction.jpg",
+		},
+		sluzby: {
+			elements: {
+				subtitle: "Služby",
+				button: "Viac o službách",
+				img: "services.jpg",
+				imgTitle: "Am Tacheles / Berlin, 2023",
+			},
+			content: [
+				{
+					title: "Stabilita",
+					content:
+						"Vďaka viac než 10-ročným skúsenostiam a dlhodobým spoluprácam s praxou overenými elektrikármi, murármi či inštalatérmi, v nás získavate stabilného partnera, ktorý promptne reaguje na akékoľvek dopyty.<br/><br/>Naša spoločnosť má všetky potrebné registrácie a povolenia na vykonávanie stavebných činností v Nemecku a v celej EÚ.",
+					img: "electrician.jpg",
+				},
+				{
+					title: "Zodpovednosť",
+					content:
+						"Sme držiteľom certifikátov kvality ISO a certifikátu dôveryhodnosti Bisnode AAA. Naša spoločnosť je prosperujúca a ekonomicky zdravá. Máme poistenie zodpovednosti proti škodám až do výšky 2,5 milióna EUR.<br/><br/>Poctivo si plníme všetky zmluvné a finančné záväzky voči úradom, klientom aj verejnosti.",
+					img: "mason.jpg",
+				},
+				{
+					title: "Skúsenosti",
+					content:
+						"Na vaše projekty vysielame ucelené partie 2-3-4 skúsených stavebných pracovníkov. Počet a zloženie radi prispôsobíme vašim požiadavkám a potrebám. Členom tímu je vždy nemecky hovoriaci vedúci pracovník.<br/><br/>Dbáme na to, aby naša činnosť bola v súlade s predpismi a legislatívou Nemecka či EÚ. Rovnaký prístup vyžadujeme od našich stavebných pracovníkov.",
+					img: "plumber.jpg",
+				},
+			],
+		},
+		referencie: {
+			elements: {
+				subtitle: "Referencie",
+				button: "Ďalšie úspešné projekty",
+			},
+			content: {
+				title: "Našou vizitkou sú spokojní partneri",
+				content:
+					"V našom portfóliu nájdete priemyselné haly, byty, apartmány, hotely, školy a komerčné budovy. Prijmeme aj tie najnáročnejšie výzvy a stavebné projekty.",
+			},
+			accordion: [
+				{
+					id: 1,
+					title: "Am Tacheles",
+					year: "2023",
+					city: "Berlin",
+					img: "Am-Tacheles.jpg",
+				},
+				{
+					id: 2,
+					title: "Mercator One",
+					year: "2016",
+					city: "Duisburg",
+					img: "Mercator-One.jpg",
+				},
+				{
+					id: 3,
+					title: "Hotel H2",
+					year: "2015",
+					city: "Leipzig",
+					img: "Hotel-H2.jpg",
+				},
+				{
+					id: 4,
+					title: "Hala",
+					year: "2018",
+					city: "Rostrock",
+					img: "Hala.jpg",
+				},
+				{
+					id: 5,
+					title: "Ubytovňa",
+					year: "2022",
+					city: "Leipzig",
+					img: "Ubytovňa.jpg",
+				},
+				{
+					id: 6,
+					title: "Výskumné centrum BV LMTK",
+					year: "2021",
+					city: "Köln",
+					img: "BV-LMTK.jpg",
+				},
+				{
+					id: 7,
+					title: "AWI Institut",
+					year: "2018",
+					city: "Potsdam",
+					img: "AWI-Institut.jpg",
+				},
+				{
+					id: 8,
+					title: "VGP Komplex",
+					year: "2020",
+					city: "Berlin",
+					img: "VGP-Komplex.jpg",
+				},
+			],
+		},
+		ktosme: {
+			elements: {
+				subtitle: "Kto sme",
+				button: "Zistite o nás viac",
+				img: "about-us.jpg",
+				imgTitle: "Am Tacheles / Berlin, 2023",
+			},
+			content: {
+				title: "Od roku 2013 sme úspešne zrealizovali 89 stavieb v 6 krajinách",
+				content:
+					"Naša stavebná firma patrí k popredným subdodávateľom pre klientov v rámci celej EÚ, no predovšetkým v Nemecku. Teší nás, že sme si za tento čas vybudovali renomé stabilného partnera, ktorý doťahuje veci do úspešného konca.<br/><br/>Zároveň ide o záväzok, ktorý si vážime a každý deň pracujeme na tom, aby sme vám poskytovali stavebné služby na najvyššej úrovni.",
+			},
+		},
+	},
+};
+export const footerData = {
+	section1: {
+		content: "Spoluprácou s našou spoločnosťou získate stabilného a skúseného partnera počas celej doby realizácie, aj na vaše ďalšie projekty",
+		button: "Kontaktujte nás",
+	},
+	section2: {
+		content: "Stabilita. Zodpovednosť. Skúsenosti.",
+		mailbutton: 'info@pasuv.de',
+		phonebutton: '+421 900 000 000',
+	},
+	section3: {
+		item1: {
+			city: "Praha",
+			address: "Ulica 00000/00 Mesto Krajina",
+			button: "Zobraziť na mape",
+		},
+		item2: {
+			city: "Prešov",
+			address: "Ulica 00000/00 Mesto Krajina",
+			button: "Zobraziť na mape",
+		},
+		item3: {
+			title: "Na stiahnutie",
+			button: "Katalóg v PDF",
+			
+		},
+		item4: {
+			title: "Sledujte nás",
+			button: "LinkedIn",
+		},
+	},
+	section4: {
+		item1: "Ochrana osobných údajov",
+		item2: "Nastavenie cookies",
+		item3: "© 2023 Pasuv EU SE. Designed & developed by SMASH",
+	},
+}
